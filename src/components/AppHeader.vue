81380619adfb64d83000f5b028d8197b88593c33
vinicius pizetta de souza <viniciuspzt@gmail.com>
2021-04-09 12:36:56 -0300

    style header, create buttons to other pages

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,37 +1,42 @@
 <template>
-  <v-app-bar
-      app
-      color="primary"
-      dark
-  >
-    <div class="d-flex align-center">
-      <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-      />
-
-      <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-      />
-    </div>
-  </v-app-bar>
+  <v-card height="50px">
+    <v-toolbar flat color="#3c3c3c">
+      <div class="d-flex align-center">
+        <v-img
+            alt="Meetime Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://meetime.com.br/dashboard/assets/images/logo-inverse.png"
+            width="100"
+        />
+      </div>
+      <v-spacer></v-spacer>
+      <v-btn @click="handleRouter('add')" plain class="link">
+        <a class="link">Add Leads</a>
+      </v-btn>
+      <v-btn @click="handleRouter('leads')" plain class="link">
+        <a class="link">List Leads</a>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
 </template>
 
 <script>
+import router from "../router";
+
 export default {
-  name: "AppHeader"
+  name: "AppHeader",
+  methods: {
+    handleRouter(route) {
+      router.push(route)
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.link {
+  color: #fff;
+}
 </style>
